caeb568b2c93be7770b5be67e3544aad0444e770
Tiago Pierre de Mendonça <43191667+tiagopierre@users.noreply.github.com>
2021-03-10 10:09:18 -0300

    applying important concepts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin react-refresh");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -9,31 +10,40 @@ module.exports = {
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx"]
   },
   devServer: {
     contentBase: path.resolve(__dirname, "public"),
+    hot: true
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-  ],
+      template: path.resolve(__dirname, "public", "index.html")
+    })
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel")
+            ].filter(Boolean)
+          }
+        }
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
 };
